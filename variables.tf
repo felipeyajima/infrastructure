294de9e28a6456a9b487aa472a3ea145bb25c2c1
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2023-12-28 21:09:07 -0300

    added variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
+variable "key" {
+  description = "access key"
+}
+
+variable "secret" {
+  description = "secret key"
+}
